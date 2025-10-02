bb290dda01059d620868ad55cc8afb5e6e9c8922
Nisrine SNACEL <nisrinesnacel@Mac.lan>
2025-10-02 20:10:44 +0200

    links added

--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -1,5 +1,7 @@
 import "./ProjectsList.css";
 import marvelImage from "../assets/img/api-marvel.png";
+import vintedImage from "../assets/img/vinted-replique.png";
+import leafImage from "../assets/img/testing-leaflet.png";
 
 const ProjectsList = () => {
   return (
@@ -16,8 +18,36 @@ const ProjectsList = () => {
           </a>
           <img src={marvelImage} alt="API Marvel" className="project-image" />
         </button>
-        <button>REPLIQUE VINTED</button>
-        <button>TRYING LEAFLET</button>
+        <button>
+          <a
+            href="https://www.linkedin.com/in/nisrine-snacel-b0a648157/"
+            className="nav-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            VINTED REPLIQUE
+          </a>
+          <img
+            src={vintedImage}
+            alt="Vinted Replique"
+            className="project-image"
+          />
+        </button>
+        <button>
+          <a
+            href="https://testing-leaf-let.netlify.app/"
+            className="nav-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TESTING LEAFLET
+          </a>
+          <img
+            src={leafImage}
+            alt="Testing Leaf Let"
+            className="project-image"
+          />
+        </button>
       </ul>
     </div>
   );
